3285155cd77d9dede8d8bad91de4f9491f28a3a0
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2018-09-22 18:17:13 +0300

    Implemented cuda rt warm up routine.

---
+++ source/cuda/init.cu
@@ -0,0 +1,39 @@
+/**
+ * init.cu
+ *
+ * Code that will trigger loading of CUDA libraries.
+ */
+
+#include <cassert>
+
+
+namespace cuda
+{
+
+__global__ void
+initKernel(float *devTable)
+{
+    float lovelyAcc = 0;
+    for (int i = 0; i < 4; ++i) lovelyAcc += *(devTable + sizeof(float) * i);
+    lovelyAcc = pow(lovelyAcc, 2.0);
+}
+
+void
+deviceInit()
+{
+    float someData[] = { 1.0, 3.0, 5.0, 10.0 };
+
+    float *devTable;
+
+    cudaError_t cudaStat = cudaMalloc(&devTable, sizeof(float) * 4);
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMemcpy(devTable, someData, 4, cudaMemcpyHostToDevice);
+    assert(cudaSuccess == cudaStat);
+
+    initKernel<<<8, 32>>>(devTable);
+
+    cudaStat = cudaFree(devTable);
+    assert(cudaSuccess == cudaStat);
+}
+
+}
---
+++ source/cuda/init.hpp
@@ -0,0 +1,19 @@
+/**
+ * init.hpp
+ *
+ */
+
+#ifndef __init_hpp__
+#define __init_hpp__
+
+
+namespace cuda
+{
+
+void
+deviceInit();
+
+}
+
+
+#endif
